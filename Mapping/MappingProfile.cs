0423ef65f05e2e9453592d732356f65407007cea
Phillip M Bentley <phillip.bentley@cgi.com>
2019-09-10 16:23:40 -0400

    Continued implementation

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using vega.Controllers.Resources;
@@ -12,10 +13,30 @@ namespace vega.Mapping
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle, VehicleResource>()
+                .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.Contact.Name, Email = v.Contact.Email, Phone = v.Contact.Phone } ))
+                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
 
             //API Resource to Domain
             CreateMap<VehicleResource, Vehicle>()
-                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature {FeatureId = id})));
+                .ForMember(v => v.Id, opt => opt.Ignore())
+                .ForMember(v => v.Features, opt => opt.Ignore())
+                .AfterMap((vr, v) => {
+                    //Find features that are no longer selected
+                    var removedFeatures = v.Features.Where(f=> !vr.Features.Contains(f.FeatureId));
+                    //Loop through removed and actually remove them
+                    foreach (var f in removedFeatures)
+                    {
+                        v.Features.Remove(f);
+                    }
+
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature {FeatureId = id});
+                    //Add new features
+                    foreach (var f in addedFeatures)
+                    {
+                        v.Features.Add(f);
+                    }
+                });
             CreateMap<ContactResource, Contact>();
         }
     }
